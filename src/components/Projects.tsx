be4d14773068c74db56db3a2e944561beb59e970
Yosman Mavarez <yosmanprecavil123@gmail.com>
2026-01-27 16:26:14 -0400

    feat: Add Projects component with interactive screenshot carousels and new image assets.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,14 +6,17 @@ import fastfood1 from "../assets/fastfood1.jpeg";
 import fastfood2 from "../assets/fastfood2.jpeg";
 import fastfood3 from "../assets/fastfood3.jpeg";
 import fastfood4 from "../assets/fastfood4.jpeg";
-import Balanzen1 from "../assets/balanzen1.jpeg";
-import Balanzen2 from "../assets/balanzen2.jpeg";
-import Balanzen3 from "../assets/balanzen3.jpeg";
-import Balanzen4 from "../assets/balanzen4.jpeg";
+import Balanzen1 from "../assets/Balanzen1.jpeg";
+import Balanzen2 from "../assets/Balanzen2.jpeg";
+import Balanzen3 from "../assets/Balanzen3.jpeg";
+import Balanzen4 from "../assets/Balanzen4.jpeg";
+import Balanzen5 from "../assets/Balanzen5.jpeg";
 import prestazo1 from "../assets/prestazo1.jpeg";
 import prestazo2 from "../assets/prestazo2.jpeg";
 import prestazo3 from "../assets/prestazo3.jpeg";
 import prestazo4 from "../assets/prestazo4.jpeg";
+import pokeapi from "../assets/pokeapi.jpeg";
+import blogApp from "../assets/blogApp.jpeg";
 
 
 import "../Styles/projects.css";
@@ -42,27 +45,45 @@ const Projects = () => {
                 ? "Aplicacion Movil para el registro de pedidos, sistema para llevar el control de los pagos y abonos, control de los platillos y precios en las monedas bolivar y dolar."
                 : "Mobile application for order registration, payment and collection control system, menu control and prices in bolivars and dollars.",
             tech: ["React Native", "Supabase", "PostgreSQL", "javascript"],
-            type: "fullstack",
+            type: "Mobile",
             screenshots: [fastfood1, fastfood2, fastfood3, fastfood4]
         },
         {
             title: "Balanzen",
             description: language === "es"
-                ? "Transforma tu relación con el dinero. Balanzen te permite registrar gastos, crear presupuestos personalizados y dar seguimiento a tus metas de ahorro, todo en una interfaz moderna y segura. Toma el control total de tu futuro financiero hoy mismo."
-                : "Transform your relationship with money. Balanzen allows you to track expenses, create personalized budgets, and monitor your savings goals, all in a modern and secure interface. Take full control of your financial future today.",
-            tech: ["React Native", "Supabase", "PostgreSQL", "javascript"],
+                ? "Transforma tu relación con el dinero. Balanzen te permite registrar gastos, crear presupuestos personalizados y dar seguimiento a tus metas de ahorro, todo en una interfaz moderna y segura. Este cuenta con su propio backend para la gestion de los datos y la seguridad de los usuarios. Y base de datos propia desplegada en Render."
+                : "Transform your relationship with money. Balanzen allows you to track expenses, create personalized budgets, and monitor your savings goals, all in a modern and secure interface. This has its own backend for data management and user security. The database is also deployed on Render.",
+            tech: ["React Native", "PostgreSQL", "typescript", "javascript", "node.js", "express.js", "Render"],
             type: "fullstack",
-            screenshots: [Balanzen1, Balanzen2, Balanzen3, Balanzen4]
+            screenshots: [Balanzen1, Balanzen2, Balanzen3, Balanzen4, Balanzen5]
         },
         {
             title: "Prestazo",
             description: language === "es"
-                ? "Aplicacion Movil para el registro de pedidos, sistema para llevar el control de los pagos y abonos, control de los platillos y precios en las monedas bolivar y dolar."
-                : "Mobile application for order registration, payment and collection control system, menu control and prices in bolivars and dollars.",
-            tech: ["React Native", "Supabase", "PostgreSQL", "javascript"],
-            type: "fullstack",
+                ? "Aplicacion Movil para el control de prestamos y deudas. Donde puedes registrar prestamos y deudas, y controlar tus pagos y abonos. Y poder visualizar a tus clientes."
+                : "Mobile application for control of loans and debts. Where you can register loans and debts, and control your payments and collections. And view your clients.",
+            tech: ["React Native", "Sqlite-expo", "javascript"],
+            type: "Mobile",
             screenshots: [prestazo1, prestazo2, prestazo3, prestazo4]
         },
+        {
+            title: "Poke Project",
+            description: language === "es"
+                ? "Api Rest para obtener datos de pokemons. Atravez de la pokeapi se obtienen los datos de los pokemons y se guardan en una base de datos MongoDB en la nube, y se muestra cuando se hace una peticion. este cuenta con un sistema de autenticacion con node.js, express.js y jwt para el control de los tokens."
+                : "Api Rest for getting pokemon data. Through the pokeapi, the pokemon data is obtained and stored in a MongoDB database in the cloud, and is displayed when a request is made. It has an authentication system with node.js, express.js and jwt for token control.",
+            tech: ["Node.js", "express.js", "MongoDB", "javascript"],
+            type: "Backend",
+            screenshots: [pokeapi]
+        },
+        {
+            title: "BlogApp",
+            description: language === "es"
+                ? "Aplicacion web para crear, editar, eliminar blogs y publicarlos para que otros usuarios puedan verlos. En proceso de volverlo responsive. cuenta con su propio backend para la gestion de los datos y la seguridad de los usuarios. Y base de datos propia local por ahora."
+                : "Web application for creating, editing and deleting blogs and publishing them so other users can view them. In the process of making it responsive. It has its own backend for data management and user security. The database is local for now.",
+            tech: ["Node.js", "express.js", "MySQL", "javascript", "typescript", "react.js"],
+            type: "fullstack",
+            screenshots: [blogApp]
+        }
     ];
 
     // Initialize active slides state
